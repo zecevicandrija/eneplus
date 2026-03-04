beb96cbfd21f95e3380971f2cb0551b36ac1ebc9
Andrija <andrija.zecevic098@gmail.com>
2026-03-04 10:54:11 +0100

    git

--- src/app/api/send-energy-passport-request/route.js
+++ src/app/api/send-energy-passport-request/route.js
@@ -1,8 +1,8 @@
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request) {
+    const resend = new Resend(process.env.RESEND_API_KEY);
     try {
         const formData = await request.formData();
 
